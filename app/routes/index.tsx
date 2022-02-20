864102cbfa8a291c55f1b3d9431c3565af3f5eec
Joanna Otmianowska <joanna.otmianowska@olx.pl>
2022-02-20 20:02:44 +0100

    styles & fonts

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,32 @@
+import type { LinksFunction } from "remix";
+import { Link } from "remix";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl
+    }
+  ];
+};
+
 export default function Index() {
   return (
-    <h1>
-      Hello Index Route
-    </h1>
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 }
